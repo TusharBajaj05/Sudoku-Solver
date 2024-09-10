aa2dd5a3379b919ab883e4ec1915535a6aa36120
Tushar <tusharbajaj.tb@gmail.com>
2024-09-10 15:48:11 -0600

    finish

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,85 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      let {puzzle, coordinate, value} = req.body;
 
+      let row = coordinate.split('')[0];
+      let col = coordinate.split('')[1];
+
+      if(/[^0-9.]/g.test(puzzle)) {
+        res.json({error: 'Invalid characters in puzzle'});
+        return
+      }
+
+      if(puzzle.length != 81) {
+        res.json({error: 'Expected puzzle to be 81 characters long'})
+        return
+      }
+
+      if(!puzzle || !coordinate || !value) {
+        res.json({error: 'Required field(s) missing'})
+        return
+      }
+      
+      if(coordinate.length !== 2 || !/[a-i]/i.test(row) || !/[1-9]/i.test(col)) {
+        res.json({error: 'Invalid coordinate'})
+      }
+
+      if(!/[1-9]/i.test(value)) {
+        res.json({error: 'Invalid value'})
+        return
+      }
+
+      let validCol = solver.checkColPlacement(puzzle, row, col, value);
+      let validRow = solver.checkRowPlacement(puzzle, row, col, value);
+      let validReg = solver.checkRegionPlacement(puzzle, row, col, value);
+      let conflicts = [];
+
+      if(validCol && validReg && validReg) {
+        res.json({valid: true});
+        return
+      }
+
+      if(!validRow)
+        conflicts.push('row');
+
+      if(!validCol)
+        conflicts.push('column');
+
+      if(!validReg)
+        conflicts.push('region');
+
+      res.json({valid: false, conflict: conflicts});
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      let puzzle = req.body.puzzle
+      let regex = /[^0-9.]/g
 
+      if(!puzzle) {
+        res.json({error: 'Required field missing'})
+        return
+      }
+
+      if(regex.test(puzzle)) {
+        res.json({ error: 'Invalid characters in puzzle' })
+        return
+      }
+
+      if(puzzle.length != 81) {
+        res.json({ error: 'Expected puzzle to be 81 characters long' })
+        return
+      }
+
+      let solvedPuzzle = solver.solve(puzzle)
+
+      console.log(solvedPuzzle)
+
+      if(!solvedPuzzle) {
+        res.json({ error: 'Puzzle cannot be solved' })
+        return
+      }
+      res.json({ solution: solvedPuzzle })
     });
 };
